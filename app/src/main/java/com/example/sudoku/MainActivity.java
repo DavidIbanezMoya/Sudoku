5fff82a968a488dd04285ca6c3d701718c028c5b
DavidIbanezMoya <davidibanezmoya@gmail.com>
2022-10-21 19:19:33 +0200

    commit 1 we createad the spinner correctly with its padding

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -15,29 +15,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        TableLayout tl = new TableLayout(this);
-
-        for (int i = 0; i < 9; i++) {
-
+        CharSequence[] nombres = {"1","2","3","4","5","6","7","8","9"};
+        TableLayout tl = findViewById(R.id.tableLayout);
+        Spinner spinnerList [][] = new Spinner[9][9];
+        for (int fila = 0; fila < 9; fila++) {
             //Es crea la fila
-            TableRow tr = new TableRow(this);
 
-            //Despres posarem el marge
-            TableRow.LayoutParams aParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+            TableRow tr = new TableRow(this);
+            for (int col = 0; col < 9; col++) {
+
 
             Spinner spinner = new Spinner(this);
-            CharSequence[] nombres = {"1","2","3","4","5","6","7","8","9"};
+                spinner.setBackground(null);
+                spinner.setPadding(5, 5, 5, 5);
+                spinner.setTag(R.id.fila,fila);
+                spinner.setTag(R.id.col,col);
             ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
                     android.R.layout.simple_spinner_item, nombres);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-            spinner.setBackground(null);
+
             spinner.setAdapter(adapter);
 
             //Afegim a la fila
             tr.addView(spinner);
 
+            }
             //Afegim la fila a la taula
             tl.addView(tr);
         }
