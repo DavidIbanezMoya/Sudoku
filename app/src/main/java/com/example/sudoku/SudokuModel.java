9ce64a670e32da706abef64046e45e372734f7a9
DavidIbanezMoya <davidibanezmoya@gmail.com>
2022-10-24 19:33:04 +0200

    commit 3 we did a pre version of the function "creaPartida" it generates some random numbers

--- app/src/main/java/com/example/sudoku/SudokuModel.java
+++ app/src/main/java/com/example/sudoku/SudokuModel.java
@@ -16,13 +16,28 @@ public class SudokuModel {
 
         //Primerament tindrem una quantitat inicial a la qual l haurem de restar
         int quantitat = 20;
-        while (quantitat > 0) {
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) {
 
+        //Per a fer l emplenat mes aleatori, generarem uns quants, primer per a saber a quina posicio i despres quina quantitat
+
+
+        while (quantitat > 0) {
+
+            int quadrant = (int) (Math.random() * (8) + 0);
+            int posicio = (int) (Math.random() * (8) + 0);
+            int numero = (int) (Math.random() * (9-1) + 1);;
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (quadrant == i) {
+                        if (posicio == j) {
+                            sudoku[i][j] = numero;
+                        }
+                    }
                 }
             }
+            quantitat--;
         }
+
     }
 
 }
