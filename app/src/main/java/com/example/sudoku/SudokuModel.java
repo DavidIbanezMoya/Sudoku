d89673d1d2013033964de7e5d4f19d077e2a4a0f
DavidIbanezMoya <davidibanezmoya@gmail.com>
2022-10-24 18:27:38 +0200

    commit 2 we createad the new class for the model

---
+++ app/src/main/java/com/example/sudoku/SudokuModel.java
@@ -0,0 +1,28 @@
+package com.example.sudoku;
+
+public class SudokuModel {
+
+    int sudoku [][] = new int [9][9];
+
+    public void setVal(int x, int y) {
+    }
+
+    public int getVal() {
+        return 0;
+    }
+
+    public void creaPartida() {
+        //La partida s ha de crear un cop tinguem per a controlar les files i columnes
+
+        //Primerament tindrem una quantitat inicial a la qual l haurem de restar
+        int quantitat = 20;
+        while (quantitat > 0) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+
+                }
+            }
+        }
+    }
+
+}
