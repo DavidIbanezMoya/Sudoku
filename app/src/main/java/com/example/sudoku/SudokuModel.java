09c03583682232760679f0dddad1cc7986f14363
David Ibáñez <73888235+DavidIbanezMoya@users.noreply.github.com>
2022-11-14 07:48:36 +0100

    Actualitzat manualment per problemes de repositori

--- app/src/main/java/com/example/sudoku/SudokuModel.java
+++ app/src/main/java/com/example/sudoku/SudokuModel.java
@@ -1,43 +1,62 @@
-package com.example.sudoku;
+package com.example.sudoku_casa;
+
+import java.util.ArrayList;
 
 public class SudokuModel {
 
     int sudoku [][] = new int [9][9];
 
-    public void setVal(int x, int y) {
-    }
-
-    public int getVal() {
-        return 0;
-    }
-
-    public void creaPartida() {
-        //La partida s ha de crear un cop tinguem per a controlar les files i columnes
-
-        //Primerament tindrem una quantitat inicial a la qual l haurem de restar
-        int quantitat = 20;
-
-        //Per a fer l emplenat mes aleatori, generarem uns quants, primer per a saber a quina posicio i despres quina quantitat
-
-
-        while (quantitat > 0) {
-
-            int quadrant = (int) (Math.random() * (8) + 0);
-            int posicio = (int) (Math.random() * (8) + 0);
-            int numero = (int) (Math.random() * (9-1) + 1);;
-
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    if (quadrant == i) {
-                        if (posicio == j) {
-                            sudoku[i][j] = numero;
-                        }
+    public void setVal(int x, int y, int numero) {
+        for (int fila = 0; fila < 8; fila++) {
+            for (int col = 0; col < 8; col++) {
+                if (x == fila) {
+                    if (y == col) {
+                        sudoku[x][y] = numero;
                     }
                 }
             }
-            quantitat--;
+        }
+    }
+
+    public int getVal(int x, int y) {
+        return sudoku[x][y];
+    }
+
+    public boolean checkRow(int filaRevisar,int columna, int number, boolean correct) {
+        for (int i = 0; i<sudoku[filaRevisar].length; i++){
+            //Comprovar que estiguen revisant el numero de la columna que toca
+            if (number==sudoku[filaRevisar][i] && sudoku[filaRevisar][i]!=0){
+                if (i!=columna)
+                    correct=false;
+            }
         }
 
+
+
+        return correct;
+    }
+
+    public boolean checkCol(int fila,int columnaRevisar, int number, boolean correct) {
+        for (int i = 0; i<sudoku.length; i++){
+            //Comprovar que estiguen revisant el numero de la columna que toca
+            if (number==sudoku[i][columnaRevisar] && sudoku[i][columnaRevisar]!=0){
+                if (i!=fila)
+                    correct=false;
+            }
+        }
+
+
+
+        return correct;
+    }
+
+    public boolean checkGeneral(int fila, int col, int number) {
+        boolean correct = true;
+
+        correct = checkRow(fila,col,number,correct);
+        correct = checkCol(fila,col,number,correct);
+
+        return correct;
     }
 
 }
